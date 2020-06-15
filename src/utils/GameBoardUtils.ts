5c4c0501fcd0e382a5d92d9bf3f5e5484d26c33c
Bressler, Tobias (TC-U) <tobias.bressler@iav.de>
2020-06-15 13:12:12 +0200

    Added utility functions.

--- src/utils/GameBoardUtils.ts
+++ src/utils/GameBoardUtils.ts
@@ -17,9 +17,30 @@ export class GameBoardUtils {
      * @return The corresponding index, between 0..63.
      */
     public static toIndex(x: number, y: number): number {
-        if ((x < 0) || (x > 7)) throw new Error("x must be between 0..7!");
-        if ((y < 0) || (y > 7)) throw new Error("y must be between 0..7!");
+        if ((!GameBoardUtils.isInt(x)) || (x < 0) || (x > 7)) throw new Error("x must be an integer and between 0..7!");
+        if ((!GameBoardUtils.isInt(y)) || (y < 0) || (y > 7)) throw new Error("y must be an integer and between 0..7!");
         return (y * 8) + x;
+    }
+
+    /**
+     * Converts the given index to the corresponding x-y coordinates.
+     *
+     * @param index The index, between 0..63.
+     * @return The corresponding x and y coordinates in the form [x, y].
+     */
+    public static toCoords(index: number): [number, number] {
+        if (!GameBoardUtils.isInt(index) || (index < 0) || (index > 63)) throw new Error("index must be an integer and between 0..63!");
+        return [(index % 8), (index - (index % 8)) / 8];
+    }
+
+    /**
+     * Checks if the given value is an integer. Returns false if it is not an integer.
+     *
+     * @param value The value to be checked.
+     * @return True if integer otherwise false.
+     */
+    public static isInt(value: number): boolean {
+        return (value === parseInt(value.toString(), 10));
     }
 
 }
--- tests/utils/GameBoardUtils.test.ts
+++ tests/utils/GameBoardUtils.test.ts
@@ -6,6 +6,27 @@ const expect = chai.expect;
 describe('GameBoardUtils class', () => {
 
     // Test:
+    it('method isInt(value) should return true for 0' , () => {
+        expect(GameBoardUtils.isInt(0)).to.equal(true);
+    });
+
+    // Test:
+    it('method isInt(value) should return true for 0.0' , () => {
+        expect(GameBoardUtils.isInt(0.0)).to.equal(true);
+    });
+
+    // Test:
+    it('method isInt(value) should return true for 2.5' , () => {
+        expect(GameBoardUtils.isInt(2.5)).to.equal(false);
+    });
+
+    // Test:
+    it('method isInt(value) should return true for 7.5' , () => {
+        expect(GameBoardUtils.isInt(7.5)).to.equal(false);
+    });
+
+
+    // Test:
     it('method toIndex(x,y) should return 0 for x=0 and y=0' , () => {
         expect(GameBoardUtils.toIndex(0, 0)).to.equal(0);
     });
@@ -23,6 +44,26 @@ describe('GameBoardUtils class', () => {
     // Test:
     it('method toIndex(x,y) should return 28 for x=4 and y=3' , () => {
         expect(GameBoardUtils.toIndex(4, 3)).to.equal(28);
+    });
+
+    // Test:
+    it('method toIndex(x,y) should throw an Error with x = 3.5 (floating point number)' , () => {
+        expect(() => GameBoardUtils.toIndex(3.5, 3)).to.throw(Error);
+    });
+
+    // Test:
+    it('method toIndex(x,y) should throw an Error with x = 6.5 (floating point number)' , () => {
+        expect(() => GameBoardUtils.toIndex(6.5, 3)).to.throw(Error);
+    });
+
+    // Test:
+    it('method toIndex(x,y) should throw an Error with y = 3.5 (floating point number)' , () => {
+        expect(() => GameBoardUtils.toIndex(3, 3.5)).to.throw(Error);
+    });
+
+    // Test:
+    it('method toIndex(x,y) should throw an Error with y = 6.5 (floating point number)' , () => {
+        expect(() => GameBoardUtils.toIndex(3, 6.5)).to.throw(Error);
     });
 
     // Test:
@@ -46,6 +87,54 @@ describe('GameBoardUtils class', () => {
     });
 
 
+    // Test:
+    it('method toCoords(index) should return [0,0] for 0' , () => {
+        expect(GameBoardUtils.toCoords(0)).to.deep.equal([0, 0]);
+    });
 
+    // Test:
+    it('method toCoords(index) should return [7,7] for 63' , () => {
+        expect(GameBoardUtils.toCoords(63)).to.deep.equal([7, 7]);
+    });
+
+    // Test:
+    it('method toCoords(index) should return [7,7] for 63' , () => {
+        expect(GameBoardUtils.toCoords(63)).to.deep.equal([7, 7]);
+    });
+
+    // Test:
+    it('method toCoords(index) should return [4,3] for 28' , () => {
+        expect(GameBoardUtils.toCoords(28)).to.deep.equal([4, 3]);
+    });
+
+    // Test:
+    it('method toCoords(index) should return [3,4] for 35' , () => {
+        expect(GameBoardUtils.toCoords(35)).to.deep.equal([3, 4]);
+    });
+
+    // Test:
+    it('method toCoords(index) should throw an error for 3.5 (floating point number)' , () => {
+        expect(() => GameBoardUtils.toCoords(3.5)).to.throw(Error);
+    });
+
+    // Test:
+    it('method toCoords(index) should throw an error for 16.5 (floating point number)' , () => {
+        expect(() => GameBoardUtils.toCoords(16.5)).to.throw(Error);
+    });
+
+    // Test:
+    it('method toCoords(index) should return [0,2] for 16.0' , () => {
+        expect(GameBoardUtils.toCoords(16.0)).to.deep.equal([0,2]);
+    });
+
+    // Test:
+    it('method toCoords(index) should throw an Error if index is lower than 0' , () => {
+        expect(() => GameBoardUtils.toCoords(-1)).to.throw(Error);
+    });
+
+    // Test:
+    it('method toCoords(index) should throw an Error if index is greater than 63' , () => {
+        expect(() => GameBoardUtils.toCoords(64)).to.throw(Error);
+    });
 
 });
