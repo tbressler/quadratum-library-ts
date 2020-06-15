78a9dd7affb9a6693da57b871f7a683ffa50d2a4
Bressler, Tobias (TC-U) <tobias.bressler@iav.de>
2020-06-15 10:17:32 +0200

    Added first Mocha/Chai tests.

---
+++ src/quadratum.ts
@@ -0,0 +1,7 @@
+export class Quadratum {
+
+    add(n1: number, n2: number): number {
+        return n1 + n2;
+    }
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.ts',
+    entry: './src/quadratum.ts',
     devtool: 'inline-source-map',
     module: {
         rules: [
